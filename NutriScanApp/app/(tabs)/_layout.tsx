709fbf3ca0e5efaef24485c7c047e06c48e7e4ed
Jacob Somphanthabansouk <202383413@collegeahuntsic.qc.ca>
2026-02-06 14:14:38 -0500

    Applique la thématique de couleur au menu inférieur

--- NutriScanApp/app/(tabs)/_layout.tsx
+++ NutriScanApp/app/(tabs)/_layout.tsx
@@ -7,43 +7,48 @@ import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Accueil',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="scanner"
-        options={{
-          title: 'Scanner',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="barcode" color={color} />,
-        }}
-      />
-        <Tabs.Screen
-            name="history"
-            options={{
-                title: 'Historique',
-                tabBarIcon: ({ color }) => <IconSymbol size={28} name="return" color={color} />,
-            }}
-        />
-        <Tabs.Screen
-            name="account"
-            options={{
-                title: 'Profil',
-                tabBarIcon: ({ color }) => <IconSymbol size={28} name="circlebadge" color={color} />,
-            }}
-        />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].icon,
+                tabBarActiveBackgroundColor: Colors[colorScheme ?? 'light'].tabBarBackgroundColor,
+                tabBarInactiveBackgroundColor: Colors[colorScheme ?? 'light'].tabBarBackgroundColor,
+
+
+                headerShown: false,
+                tabBarButton: HapticTab,
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Accueil',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="scanner"
+                options={{
+                    title: 'Scanner',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="barcode" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="history"
+                options={{
+                    title: 'Historique',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="return" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="account"
+                options={{
+                    title: 'Profil',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="circlebadge" color={color} />,
+                }}
+            />
+        </Tabs>
+    );
 }
