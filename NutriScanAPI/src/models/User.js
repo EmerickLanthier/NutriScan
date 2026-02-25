294186b54bffc8b133ca199543c7c54540b4f019
madridbg <madrid.bouting@gmail.com>
2026-02-25 10:14:40 -0500

    feat: création du modèle User (Mongoose) pour la base de données

--- NutriScanAPI/src/models/User.js
+++ NutriScanAPI/src/models/User.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, trim: true },
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
     password_hash: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
     favorite_products: [{ type: String, ref: 'Product' }],
-    favorite_recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }]
+    favorite_recipes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Recipe' }],
+    reset_token: { type: String, default: null }
 });
 
 module.exports = mongoose.model('User', userSchema);
