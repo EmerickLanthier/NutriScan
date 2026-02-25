fe299d0d07cb35d420969198aa43220620327732
madridbg <madrid.bouting@gmail.com>
2026-02-25 10:14:26 -0500

    feat: ajout de la configuration de base du serveur et connexion à MongoDB Atlas

--- NutriScanAPI/src/server.js
+++ NutriScanAPI/src/server.js
@@ -1,8 +1,24 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URI;
+const app = express();
 
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri)
     .then(() => console.log("Connecté à MongoDB Atlas !"))
     .catch(err => console.error("Erreur de connexion :", err));
+
+const authRoutes = require('../src/routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API NutriScan en ligne !');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré et en écoute sur le port ${PORT}`);
+});
