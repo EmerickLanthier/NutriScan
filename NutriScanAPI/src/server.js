09813bdc51fe945170c2ab7eb8b69428e44e5950
Jacob Somphanthabansouk <202383413@collegeahuntsic.qc.ca>
2026-02-26 22:21:36 -0500

    feat: Utilisation des routes dans le server.js

--- NutriScanAPI/src/server.js
+++ NutriScanAPI/src/server.js
@@ -1,8 +1,23 @@
+const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
-const uri = process.env.MONGO_URI;
+const productRoutes = require('./routes/product.routes');
 
-mongoose.connect(uri)
-    .then(() => console.log("Connecté à MongoDB Atlas !"))
-    .catch(err => console.error("Erreur de connexion :", err));
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connecté à MongoDB Atlas (nutriscan_db)'))
+    .catch((err) => console.error('Erreur de connexion MongoDB:', err));
+
+app.use('/api/product', productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
